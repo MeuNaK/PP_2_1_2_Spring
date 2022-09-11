3a29b1496569193c99353290d29798748b6f7b5f
MeuNaK <ahmeddilda@gmail.com>
2022-09-11 19:17:57 +0500

    It is good?

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        System.out.println(bean1 == bean2);
+        Cat beanCat1 =applicationContext.getBean("cat", Cat.class);
+        System.out.println(beanCat1.getName());
+        Cat beanCat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(beanCat2.getName());
+        System.out.println(beanCat1 == beanCat2);
     }
 }
